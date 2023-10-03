f3ffe17269de82dd5468217c69c115c43984eb48
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-10-04 00:22:34 +0530

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -149,9 +149,9 @@ const About = () => {
             {/* EXPERIENCE */}
             <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
               <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
-                <CountUp start={0} end={10} duration={5} /> +
+                <CountUp start={0} end={200000} duration={5} />+
               </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Years of Experience</div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Prize Pool</div>
             </div>
           </div>
         </div>
