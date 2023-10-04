81f8b88a8f34343f4f28a6000126a1103f9e5b6c
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-10-05 01:07:41 +0530

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -17,7 +17,7 @@ const Socials = () => {
     <Link href={'https://jaipur.manipal.edu/'} className=" hover:text-accent translate-all duration 300">
       <RiDribbbleLine />
     </Link>
-    <Link href={'/Register'} className=" hover:text-accent translate-all duration 300">
+    <Link href={'/register'} className=" hover:text-accent translate-all duration 300">
       <button className="bg-transparent hover:text-accent text-white font-semibold hover:text-accent py-2 px-4 border border-transparent hover:border-teansparent rounded">Register</button>
     </Link>
     {/* <Link href={' '} className=" hover:text-accent translate-all duration 300">
