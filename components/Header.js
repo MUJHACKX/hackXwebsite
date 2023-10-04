08ec7299ac23bdb0a10b8a9908f8e79dec0e9e23
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-10-04 17:00:45 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 //components
-import Socials from '../components/Socials';
+// import Socials from '../components/Socials';
 
 const Header = () => {
   return <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
