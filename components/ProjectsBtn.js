62607031d6780e8d6fb2707a252e5b5012af26b1
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-10-04 17:19:13 +0530

    Update ProjectsBtn.js

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -11,7 +11,7 @@ import {HiArrowRight} from 'react-icons/hi2';
 const ProjectsBtn = () => {
   return(
   <div className="mx-auto xl:mx-0">
-    <Link href={'/register'} 
+    <Link href={'/Register'} 
     className="reletive w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
       <Image src={'/roundedText.png'} width={141} height={148} alt='' 
       className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
