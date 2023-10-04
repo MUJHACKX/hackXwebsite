84af1494269272bd979b54cab1ed930dc0c373e4
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-10-05 00:56:54 +0530

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -1,34 +1,34 @@
-// //links
-// import Link from "next/link"
+//links
+import Link from "next/link"
 
-// //icons
-// import {RiYoutubeLine, RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiPinterestLine, RiLinkedinLine} from 'react-icons/ri'
-// import {HiArrowRight} from 'react-icons/hi2';
+//icons
+import {RiYoutubeLine, RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiPinterestLine, RiLinkedinLine} from 'react-icons/ri'
+import {HiArrowRight} from 'react-icons/hi2';
 
 
-// const Socials = () => {
-//   return <div className="flex items-center gap-x-5 mb-8 text-lg">
-//     {/* <Link href={' '} className=" hover:text-accent translate-all duration 300">
-//       <RiYoutubeLine />
-//     </Link> */}
-//     <Link href={' '} className=" hover:text-accent translate-all duration 300">
-//       <RiInstagramLine />
-//     </Link>
-//     <Link href={' '} className=" hover:text-accent translate-all duration 300">
-//       <RiDribbbleLine />
-//     </Link>
-//     <Link href={'/register'} className=" hover:text-accent translate-all duration 300">
-//       <button className="bg-transparent hover:text-accent text-white font-semibold hover:text-accent py-2 px-4 border border-transparent hover:border-teansparent rounded">Register</button>
-//     </Link>
-//     {/* <Link href={' '} className=" hover:text-accent translate-all duration 300">
-//       <RiLinkedinLine />
-//     </Link>
-//     <Link href={' '} className=" hover:text-accent translate-all duration 300">
-//       <RiPinterestLine />
-//     </Link> */}
+const Socials = () => {
+  return <div className="flex items-center gap-x-5 mb-8 text-lg">
+    {/* <Link href={' '} className=" hover:text-accent translate-all duration 300">
+      <RiYoutubeLine />
+    </Link> */}
+    <Link href={' '} className=" hover:text-accent translate-all duration 300">
+      <RiInstagramLine />
+    </Link>
+    <Link href={' '} className=" hover:text-accent translate-all duration 300">
+      <RiDribbbleLine />
+    </Link>
+    <Link href={'/register'} className=" hover:text-accent translate-all duration 300">
+      <button className="bg-transparent hover:text-accent text-white font-semibold hover:text-accent py-2 px-4 border border-transparent hover:border-teansparent rounded">Register</button>
+    </Link>
+    {/* <Link href={' '} className=" hover:text-accent translate-all duration 300">
+      <RiLinkedinLine />
+    </Link>
+    <Link href={' '} className=" hover:text-accent translate-all duration 300">
+      <RiPinterestLine />
+    </Link> */}
     
 
-//   </div>;
-// };
+  </div>;
+};
 
-// export default Socials;
+export default Socials;
