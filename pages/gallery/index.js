d35b337178c82c9e0229af2f567b8a2d08472a0e
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-15 23:12:20 +0530

    added the team hackx section

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -3,6 +3,24 @@ import { fadeIn } from '../../variants'
 import {motion} from 'framer-motion'
 import  SwipeCarousel  from "../../components/carousel"
 
+
+
+const members = [{
+  personImage: "/preet.jpg",
+  personName:"Preet"
+},{
+  personImage:"/Dev.jpg",
+  personName:"Dev Shalinkumar Agrawal"
+},{
+  personImage: "/Ayush.jpg",
+  personName:"Aneerban Saha"
+},{
+  personImage: "/Aneerban.jpg",
+  personName:"Aneerban Saha"
+},{
+  personImage: "/Lakshay.png",
+  personName:"Lakshay Goel"
+},]
 const gallery = ()=>{
     return <div className='font bg-[#131424] h-auto '>
                 <div className="flex justify-center  ">
@@ -16,9 +34,34 @@ const gallery = ()=>{
         initial="hidden" 
         animate="show"
         exit="hidden"
-        className="h3 text-center text-[#d1d3e3] mt-[30px] sm:mb-[200px] mb-[150px]  "> 
+        className="h3 text-center text-[#d1d3e3] mt-[30px] mb-[40px]"> 
           Team HackX
         </motion.h3>
+        {members.length > 0 && (
+  <>
+    <div className="flex flex-wrap justify-around  bg-primary  sm:mb-[200px] mb-[150px] ">
+      {members.map((member, index) => (
+        <div key={index}>
+         <div className="relative overflow-hidden transition duration-300 transform rounded-lg  hover:scale-105 px-2 py-4 hover:shadow-3xl hover:-translate-y-2 ">
+            <Image
+              className="object-cover rounded-lg w-64  h-64 lg:w-64 lg:h-64"
+              src={member.personImage}
+              width={700}
+              height={1000}
+              alt={member.personName}
+            ></Image>
+            <div className=" text-center py-4 hover:text-sky-400">
+              <p className="mb-4 text-xl font-bold text-[#d1d3e3]">{member.personName}</p>
+              {/* <p className="mb-4 text-lg lg:text-base text-wheat">{member.personPosition}</p> */}
+            </div>
+             </div>
+             
+         
+        </div>
+      ))}
+    </div>
+  </>
+)}
             <motion.h3 
         variants={fadeIn('down', 0.4)}
         initial="hidden" 
