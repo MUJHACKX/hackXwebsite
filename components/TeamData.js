4c52e10711496af5c78781b1deae6c3ffa3e4f4d
Khushveer malviya <115608753+khushveermalviya@users.noreply.github.com>
2024-05-26 10:10:06 +0530

    Update TeamData.js

--- components/TeamData.js
+++ components/TeamData.js
@@ -34,35 +34,35 @@ const TeamData = [
         personName: "Dr. Arun Shanbhag",
         personPosition: "CO-PATRON",
         personOrganization: "DEAN FOE,MUJ",
-        personLinkedIn: "https://www.linkedin.com/in/pujan-modha/"
+        personLinkedIn: "https://www.linkedin.com/in/arunshanbhag/"
     },
     {
         personImage: '/SandeepCaurasia.jpg',
         personName: "Dr. Sandeep Chaurasia",
         personPosition: "GENERAL CHAIR",
         personOrganization: "DIRECTOR SCSE,MUJ",
-        personLinkedIn: "https://www.linkedin.com/in/pujan-modha/"
+        personLinkedIn: "https://www.linkedin.com/in/sandeep-chaurasia-0457568a/"
     },
     {
         personImage: '/AmitSoni.jpg',
         personName: "Dr. Amit Soni",
         personPosition: "CEO AIC",
         personOrganization: "MUJ",
-        personLinkedIn: "https://www.linkedin.com/in/pujan-modha/"
+        personLinkedIn: "https://www.linkedin.com/in/dr-amit-soni-9b810812/"
     },
     {
         personImage: '/DineshSaini.jpg',
         personName: "Dr. Dinesh Saini",
         personPosition: "DIRECTOR, ECELL",
         personOrganization: "MUJ",
-        personLinkedIn: "https://www.linkedin.com/in/pujan-modha/"
+        personLinkedIn: "https://www.linkedin.com/in/dinesh-saini-4354028/"
     },
     {
         personImage: '/NehaChaudhary.jpg',
         personName: "Dr. Neha Chaudhary",
         personPosition: "PROGRAM CHAIR",
         personOrganization: "HOD CSE,MUJ",
-        personLinkedIn: "https://www.linkedin.com/in/pujan-modha/"
+        personLinkedIn: "https://www.linkedin.com/in/dr-neha-chaudhary-2b02bb31/"
     },
     {
         personImage: '/SantoshKumarVishwakarma.jpg',
@@ -76,14 +76,14 @@ const TeamData = [
         personName: "Dr. Juhi Singh",
         personPosition: "Convener",
         personOrganization: "MUJ",
-        personLinkedIn: "https://www.linkedin.com/in/pujan-modha/"
+        personLinkedIn: "https://www.linkedin.com/in/dr-juhi-singh-171a4873/"
     },
     {
         personImage: '/ShishirSinghChauhan.jpg',
         personName: "Mr. Shishir Singh",
         personPosition: "Convener",
         personOrganization: "MUJ",
-        personLinkedIn: "https://www.linkedin.com/in/pujan-modha/"
+        personLinkedIn: "https://www.linkedin.com/in/shishir-chauhan-b888b2129/"
     },
 ];
 
