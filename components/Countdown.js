35748d5bef8cae3d74c4173989472a823ce02d82
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-06-03 01:28:26 +0530

    Redesign the home page

---
+++ components/Countdown.js
@@ -0,0 +1,35 @@
+"use client";
+
+import {useEffect, useState} from "react";
+import FlipCard from "./FlipCard";
+
+const Countdown = ({ date }) => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  // update the value every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = (date - now) / 1000;
+      setDays(Math.floor(distance / (60 * 60 * 24)));
+      setHours(Math.floor((distance % (60 * 60 * 24)) / (60 * 60)));
+      setMinutes(Math.floor((distance % (60 * 60)) / 60));
+      setSeconds(Math.floor(distance % 60));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex justify-center gap-2 text-white select-none cursor-default">
+      <FlipCard value={days} heading="Days" />
+      <FlipCard value={hours} heading="Hours" />
+      <FlipCard value={minutes} heading="Minutes" />
+      <FlipCard value={seconds} heading="Seconds" />
+    </div>
+  );
+}
+
+export default Countdown;
---
+++ components/RegisterButton.js
@@ -0,0 +1,71 @@
+import {motion} from 'framer-motion';
+import {FaArrowRight} from "react-icons/fa";
+
+const ANIM_STATES = {
+  initial: {
+    width: '0%',
+  },
+  whileHover: {
+    width: '100%',
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+export default function RegisterButton() {
+  return (
+    <motion.a
+      href="https://linktr.ee/mujhackx"
+      target="_blank"
+      variants={{
+        whileHover: {
+          backgroundColor: '#5256a2',
+        },
+      }}
+      initial="initial"
+      whileHover="whileHover"
+      className="hover:cursor-pointer relative h-14 border border-[#5256a2] flex overflow-hidden bg-slate-100 py-3
+      gap-2 pr-4 pl-2 text-black rounded-full uppercase font-medium leading-loose poppins md:text-xl"
+    >
+      <motion.div
+        variants={ANIM_STATES}
+        className="rounded-full h-full w-full bg-[#5256a2] -ml-2 absolute top-0"
+      ></motion.div>
+      <div className="block relative">
+        <motion.div
+          variants={{
+            initial: {
+              y: 0,
+              opacity: 1,
+            },
+            whileHover: {
+              y: -50,
+              opacity: 0,
+            },
+          }}
+          className="flex z-10 items-center gap-2"
+        >
+          <FaArrowRight/>
+          <span className="z-10">Register Now</span>
+        </motion.div>
+        <motion.div
+          variants={{
+            initial: {
+              y: 0,
+              opacity: 0,
+            },
+            whileHover: {
+              y: -27,
+              opacity: 1,
+            },
+          }}
+          className="flex z-10 items-center gap-2 text-white"
+        >
+          <FaArrowRight />
+          <span className="z-10">Register Now</span>
+        </motion.div>
+      </div>
+    </motion.a>
+  );
+}
