ffc50660e69db8ff374d9331e1ff6950a15f3607
Khushveer malviya <115608753+khushveermalviya@users.noreply.github.com>
2024-06-03 10:39:19 +0530

    Update FacultyCard.js
    remove extra margin

--- components/FacultyCard.js
+++ components/FacultyCard.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
+import { FaLinkedin } from "react-icons/fa";
 
-const FacultyCard = ({ name, image, line1, line2, count }) => {
+const FacultyCard = ({ name, image, line1, line2, count, icon }) => {
   return (
     <div className={`flex flex-col items-center w-full md:w-[${(100/count).toFixed(0)}%]`}>
       <div
@@ -18,6 +19,9 @@ const FacultyCard = ({ name, image, line1, line2, count }) => {
         <h1 className="mb-4 text-xl 2xl:text-3xl font-bold text-white">{name}</h1>
         <h2 className="mb-2 md:text-lg lg:text-base text-white">{line1}</h2>
         <h3 className="mb-4 md:text-lg lg:text-base text-white">{line2}</h3>
+      </div>
+      <div className="flex justify-center mb-3  cursor-pointer">
+           <a href={icon} target="_blank" rel="noopener noreferrer"><FaLinkedin  size="1.5em" className="ml-2 hover:text-blue-500" /></a>
       </div>
     </div>
   );
