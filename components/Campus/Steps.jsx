f05298f2615ce82a20b248d489d45248e62584fa
Roshmeet Chakraborty <75925727+C0D3R-M33T@users.noreply.github.com>
2024-06-18 19:48:30 +0530

    Update Steps.jsx

--- components/Campus/Steps.jsx
+++ components/Campus/Steps.jsx
@@ -17,7 +17,7 @@ const Steps = () => {
           <div className="flex items-center p-4 bg-white shadow-md rounded-lg">
             <div className="flex items-center justify-center w-12 h-12 mr-4 text-2xl font-bold text-white bg-blue-600 rounded-full">2</div>
             <div>
-              <p className="font-semibold text-black">Take the assessment.</p>
+              <p className="font-semibold text-black">Once you recieve a mail from us you are officially a MujHackX2.0 Campus Ambassador!</p>
             </div>
           </div>
         </div>
