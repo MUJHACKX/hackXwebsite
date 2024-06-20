97f5494e25401b03d771db2fa61a1e1869ac1d5d
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2024-06-21 00:54:56 +0530

    Update Main.jsx

--- components/Campus/Main.jsx
+++ components/Campus/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
         <div className="text-center px-4 sm:px-6 lg:px-8">
           <h1 className="text-white text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 bg-gradient-to-r from-amber-200 via-amber-200 to-amber-400 bg-clip-text text-transparent  ">Campus Ambassador <span className='bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent'  >MujHackx</span></h1>
           <p className="text-white text-lg sm:text-xl lg:text-2xl mb-8">
-            Be the <RotatingText items={words} /> of our campus!
+            Be the <RotatingText items={words} /> of your campus!
         </p><a className="bg-gradient-to-r from-violet-600 to-indigo-600 text-white py-2 px-4 rounded-lg hover:bg-blue-800 transition duration-300"
    href="https://forms.gle/WgQjJWdB7bgGH6fw5"
    role="button"
