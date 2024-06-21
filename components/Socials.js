d32e781e50b0a3ba878fa11495e8736de73b7f93
Roshmeet Chakraborty <75925727+C0D3R-M33T@users.noreply.github.com>
2024-06-21 18:21:51 +0530

    Added Linkedin

--- components/Socials.js
+++ components/Socials.js
@@ -1,33 +1,27 @@
 import Link from "next/link";
-import {RiInstagramLine, RiDribbbleLine} from 'react-icons/ri';
+import { RiInstagramLine, RiDribbbleLine, RiLinkedinLine } from 'react-icons/ri';
 import LinkButton from "./LinkButton";
 
 const problemStatementsReleased = false;
 
 const Socials = () => {
-  return <div className="flex items-center gap-x-5 mb-8 text-lg">
-    {/* <Link href={' '} className=" hover:text-accent translate-all duration 300">
-      <RiYoutubeLine />
-    </Link> */}
-    <Link href={'https://instagram.com/muj.hackx?igshid=MzRlODBiNWFlZA=='} target="_blank"
-          className=" hover:text-accent translate-all duration 300">
-      <RiInstagramLine/>
-    </Link>
-    <Link href={'https://jaipur.manipal.edu/'} target="_blank"
-          className=" hover:text-accent translate-all duration 300">
-      <RiDribbbleLine/>
-    </Link>
-    <LinkButton ps_released={problemStatementsReleased}/>
-    {/*<Link href={'https://linktr.ee/mujhackx'} target="_blank" className=" hover:text-accent translate-all duration 300">*/}
-    {/*  <button className="bg-transparent hover:text-accent text-white font-semibold hover:text-accent py-2 px-4 border border-transparent hover:border-teansparent rounded">Register</button>*/}
-    {/*</Link>*/}
-    {/* <Link href={' '} className=" hover:text-accent translate-all duration 300">
-      <RiLinkedinLine />
-    </Link>
-    <Link href={' '} className=" hover:text-accent translate-all duration 300">
-      <RiPinterestLine />
-    </Link> */}
-  </div>;
+  return (
+    <div className="flex items-center gap-x-5 mb-8 text-lg">
+      <Link href={'https://www.linkedin.com/company/mujhackx'} target="_blank"
+            className="hover:text-accent translate-all duration 300">
+        <RiLinkedinLine />
+      </Link>
+      <Link href={'https://instagram.com/muj.hackx?igshid=MzRlODBiNWFlZA=='} target="_blank"
+            className="hover:text-accent translate-all duration 300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={'https://jaipur.manipal.edu/'} target="_blank"
+            className="hover:text-accent translate-all duration 300">
+        <RiDribbbleLine />
+      </Link>
+      <LinkButton ps_released={problemStatementsReleased} />
+    </div>
+  );
 };
 
 export default Socials;
