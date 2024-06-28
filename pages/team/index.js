14d43e47110e5bd1c80b87fbbaa8df87ef7d7c07
Roshmeet Chakraborty <75925727+C0D3R-M33T@users.noreply.github.com>
2024-06-29 01:44:41 +0530

    mobile responsive

--- pages/team/index.js
+++ pages/team/index.js
@@ -3,7 +3,7 @@ import TeamData from "../../data/teamData";
 import FacultyCard from "../../components/FacultyCard";
 
 const Team = () => {
-  const [year, setYear] = useState(2024);
+  const [year, setYear] = useState(2024);  // Default to 2024
 
   const chiefPatron = TeamData.find((team) => team.personPosition === "CHIEF PATRON");
   const patron = TeamData.find((team) => team.personPosition === "PATRON");
@@ -44,53 +44,31 @@ const Team = () => {
       team.personName === "Mr. Shishir Singh"
   );
 
-  const StudentConvener1 = [{
-    personImage: "/preet.jpg",
-    personName: "Preet"
-  }, {
-    personImage: "/Dev.jpg",
-    personName: "Dev Shalinkumar Agrawal"
-  }, {
-    personImage: "/Ayush.jpg",
-    personName: "Ayush Poddar"
-  }]
+  const StudentConvener1 = [
+    { personImage: "/preet.jpg", personName: "Preet" },
+    { personImage: "/Dev.jpg", personName: "Dev Shalinkumar Agrawal" },
+    { personImage: "/Ayush.jpg", personName: "Ayush Poddar" },
+  ];
 
-  const StudentConvener2 = [{
-    personImage: "/Aneerban.jpg",
-    personName: "Aneerban Saha"
-  }, {
-    personImage: "/Lakshay.png",
-    personName: "Lakshya Goel"
-  }, {
-    personImage: "/Manya.jpg",
-    personName: "Manya Goel"
-  }]
+  const StudentConvener2 = [
+    { personImage: "/Aneerban.jpg", personName: "Aneerban Saha" },
+    { personImage: "/Lakshay.png", personName: "Lakshya Goel" },
+    { personImage: "/Manya.jpg", personName: "Manya Goel" },
+  ];
 
-  const StudentConvener2024_25 = [{
-    personImage: "/naad.png",
-    personName: "Naad Dantale"
-  }, {
-    personImage: "/rishi.png",
-    personName: "Rishi Joshi"
-  }, {
-    personImage: "/krishna.png",
-    personName: "Srishti Krishna"
-  }, {
-    personImage: "/pulkit.png",
-    personName: "Pulkit Gupta"
-  }, {
-    personImage: "/roshmeet.png",
-    personName: "Roshmeet Chakraborty"
-  }, {
-    personImage: "/image 1.png",
-    personName: "Abhijeet Anand Jha"
-  }]
+  const StudentConvener2024_25 = [
+    { personImage: "/naad.png", personName: "Naad Dantale" },
+    { personImage: "/rishi.png", personName: "Rishi Joshi" },
+    { personImage: "/krishna.png", personName: "Srishti Krishna" },
+    { personImage: "/pulkit.png", personName: "Pulkit Gupta" },
+    { personImage: "/roshmeet.png", personName: "Roshmeet Chakraborty" },
+    { personImage: "/image 1.png", personName: "Abhijeet Anand Jha" },
+  ];
 
   return (
     <div className="bg-primary flex items-center justify-center pb-[30%] md:pb-[10%] relative z-10">
       <div className="w-full md:w-[80%]">
-        <h2 className="h2 text-center mb-12 text-accent pt-[20vh] font-bold bg-gradient-to-r ease-in-out via-purple-500 bg-clip-text text-transparent
-            from-indigo-300 to-indigo-300 animate-text">
+        <h2 className="h2 text-center mb-12 text-accent pt-[20vh] font-bold bg-gradient-to-r ease-in-out via-purple-500 bg-clip-text text-transparent from-indigo-300 to-indigo-300 animate-text">
           {year === 2023 ? "Team HackX 2023" : "Team HackX 2024"}
         </h2>
 
@@ -131,85 +109,69 @@ const Team = () => {
         </div>
 
         <h2 className="text-center text-4xl font-bold my-4 text-white bg-primary">Co-Patron</h2>
-        {(
-          <>
-            <hr className="mb-4" />
-            <div className="flex flex-col lg:flex-row items-center">
-              {(year === 2023 ? coPatrons2023 : coPatrons2024).map((coPatron, index) => (
-                <FacultyCard
-                  key={index}
-                  name={coPatron.personName}
-                  image={coPatron.personImage}
-                  line1={coPatron.personPosition}
-                  line2={coPatron.personOrganization}
-                  count={coPatrons2023.length}
-                />
-              ))}
-            </div>
-          </>
-        )}
+        <hr className="mb-4" />
+        <div className="flex flex-col lg:flex-row items-center">
+          {(year === 2023 ? coPatrons2023 : coPatrons2024).map((coPatron, index) => (
+            <FacultyCard
+              key={index}
+              name={coPatron.personName}
+              image={coPatron.personImage}
+              line1={coPatron.personPosition}
+              line2={coPatron.personOrganization}
+              count={coPatrons2023.length}
+            />
+          ))}
+        </div>
 
         <h2 className="text-center text-4xl font-bold my-4 text-white bg-primary">Chair</h2>
-        {(
-          <>
-            <hr className="mb-4 bg-primary" />
-            <div className="flex flex-col lg:flex-row items-center">
-              {(year === 2023 ? secondLineMembers2023 : secondLineMembers2024).map((member, index) => (
-                <FacultyCard
-                  key={index}
-                  name={member.personName}
-                  image={member.personImage}
-                  line1={member.personPosition}
-                  line2={member.personOrganization}
-                  count={secondLineMembers2024.length}
-                />
-              ))}
-            </div>
-          </>
-        )}
+        <hr className="mb-4" />
+        <div className="flex flex-col lg:flex-row items-center">
+          {(year === 2023 ? secondLineMembers2023 : secondLineMembers2024).map((member, index) => (
+            <FacultyCard
+              key={index}
+              name={member.personName}
+              image={member.personImage}
+              line1={member.personPosition}
+              line2={member.personOrganization}
+              count={secondLineMembers2024.length}
+            />
+          ))}
+        </div>
 
         <h2 className="text-center text-4xl font-bold my-4 text-white bg-primary">Program Chair</h2>
-        {(
-          <>
-            <hr className="mb-4 bg-primary" />
-            <div className="flex flex-col md:flex-row items-center">
-              {(year === 2023 ? thirdLineMembers2023 : thirdLineMembers2024).map((member, index) => (
-                <FacultyCard
-                  key={index}
-                  name={member.personName}
-                  image={member.personImage}
-                  line1={member.personPosition}
-                  line2={member.personOrganization}
-                  count={thirdLineMembers2024.length}
-                />
-              ))}
-            </div>
-          </>
-        )}
+        <hr className="mb-4" />
+        <div className="flex flex-col md:flex-row items-center">
+          {(year === 2023 ? thirdLineMembers2023 : thirdLineMembers2024).map((member, index) => (
+            <FacultyCard
+              key={index}
+              name={member.personName}
+              image={member.personImage}
+              line1={member.personPosition}
+              line2={member.personOrganization}
+              count={thirdLineMembers2024.length}
+            />
+          ))}
+        </div>
 
         <h2 className="text-center text-4xl font-bold my-4 text-white bg-primary">Convener</h2>
-        {(
-          <>
-            <hr className="mb-4 bg-primary" />
-            <div className="flex flex-wrap justify-center md:justify-around bg-primary ">
-              {fifthLineMembers.map((member, index) => (
-                <FacultyCard
-                  key={index}
-                  name={member.personName}
-                  image={member.personImage}
-                  line1={member.personPosition}
-                  line2={member.personOrganization}
-                  count={fifthLineMembers.length}
-                />
-              ))}
-            </div>
-          </>
-        )}
+        <hr className="mb-4" />
+        <div className="flex flex-wrap justify-center md:justify-around bg-primary ">
+          {fifthLineMembers.map((member, index) => (
+            <FacultyCard
+              key={index}
+              name={member.personName}
+              image={member.personImage}
+              line1={member.personPosition}
+              line2={member.personOrganization}
+              count={fifthLineMembers.length}
+            />
+          ))}
+        </div>
 
         {year === 2023 ? (
           <>
             <h2 className="text-center text-2xl md:text-4xl font-bold my-4 text-white bg-primary">Student Convener 2023-24</h2>
-            <hr className="mb-4 bg-primary" />
+            <hr className="mb-4" />
             <div className="flex flex-col lg:flex-row items-center">
               {StudentConvener1.map((member, index) => (
                 <FacultyCard
@@ -238,10 +200,10 @@ const Team = () => {
         ) : (
           <>
             <h2 className="text-center text-2xl md:text-4xl font-bold my-4 text-white bg-primary">Student Convener 2024-25</h2>
-            <hr className="mb-4 bg-primary" />
-            <div className="flex flex-wrap justify-center md:justify-around bg-primary">
+            <hr className="mb-4" />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center bg-primary">
               {StudentConvener2024_25.map((member, index) => (
-                <div key={index} className="flex flex-col items-center w-[30%] p-4">
+                <div key={index} className="flex flex-col items-center">
                   <FacultyCard
                     name={member.personName}
                     image={member.personImage}
