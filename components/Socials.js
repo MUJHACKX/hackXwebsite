0e9a6b1b4aa628f779388ad6cc13546512d02524
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-30 11:44:05 +0530

    removed the buttons from socials

--- components/Socials.js
+++ components/Socials.js
@@ -6,21 +6,21 @@ const problemStatementsReleased = false;
 
 const Socials = () => {
   return (
-    <div className="flex items-center gap-x-5 mb-8 text-lg">
+    <div className="flex items-center gap-x-5 mb-8 text-2xl">
       <Link href={'https://www.linkedin.com/company/mujhackx'} target="_blank"
-            className="hover:text-accent translate-all duration 300">
+        className="hover:text-accent translate-all duration 300">
         <RiLinkedinLine />
       </Link>
       <Link href={'https://instagram.com/muj.hackx?igshid=MzRlODBiNWFlZA=='} target="_blank"
-            className="hover:text-accent translate-all duration 300">
+        className="hover:text-accent translate-all duration 300">
         <RiInstagramLine />
       </Link>
       <Link href={'https://jaipur.manipal.edu/'} target="_blank"
-            className="hover:text-accent translate-all duration 300">
+        className="hover:text-accent translate-all duration 300">
         <RiDribbbleLine />
       </Link>
-      <LinkButton ps_released={problemStatementsReleased} />
-    </div>
+      { /* <LinkButton ps_released={problemStatementsReleased} />
+    */}</div>
   );
 };
 
