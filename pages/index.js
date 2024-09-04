41ac3871e506b76075315a429823d811745ef20b
C0D3R-M33T <riju17chak@gmail.com>
2024-09-04 10:30:16 +0530

    oracle updated

--- pages/index.js
+++ pages/index.js
@@ -418,7 +418,7 @@ const Home = () => {
           {/* Sponsor 1 */}
           <div className="flex flex-col items-center">
             <img
-              src="/sponsors/Oracle.png"
+              src="/sponsors/oracleoriginal.png"
               alt="Title Sponsor 1"
               className="w-full"
             />
